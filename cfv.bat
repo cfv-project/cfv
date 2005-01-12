6c3f4c003a841b8d156f433385a8acd4d302f864
Matthew Mueller <donut@users.sourceforge.net>
2005-01-12 08:22:58 +0000

    recommend using %* on win nt/2k/xp

--- cfv.bat
+++ cfv.bat
@@ -1,2 +1,6 @@
 @echo off
-c:\python24\python -c "import cfv; cfv.main()" %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem use this line for old windows (95,98,ME)
+rem c:\python24\python -c "import cfv; cfv.main()" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+rem use this line for new windows (NT, 2000, XP, etc)
+c:\python24\python -c "import cfv; cfv.main()" %*
