87624a296eee88d82faef1139d91d3c85f5ed4ff
David Gnedt <david@gnedt.at>
2019-03-11 00:51:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,61 @@
 #! /usr/bin/env python
 
-from distutils.core import setup
-try:
-	import py2exe
-	extrakws = dict(console=["bin/cfv"])
-except ImportError:
-	extrakws = {}
+import re
+from setuptools import setup, find_packages
 
-setup(name="cfv",
-	package_dir = {'': 'lib'},
-	packages = ['cfv'],
-	scripts = ['bin/cfv'],
-	data_files = [("man/man1", ["cfv.1"])],
-	options={"py2exe": {"packages": ["encodings"]}},
-	version="2.0.x",
-	author="Matthew Mueller",
-	license="GPL",
-	url="http://cfv.sourceforge.net",
-	**extrakws
+
+RE_VERSION = "^__version__\s*=\s*'([^']*)'$"
+
+
+def _read(path):
+	with open(path, 'r') as f:
+		return f.read()
+
+
+def _get_version(path):
+	re_version = re.compile(RE_VERSION)
+	with open(path, 'r') as f:
+		for line in f:
+			m = re_version.match(line)
+			if m is not None:
+				return m.group(1)
+	return None
+
+
+version = _get_version('lib/cfv/common.py')
+
+setup(
+	name='cfv',
+	version=version,
+	description='Command-line File Verify - versatile file checksum creator and verifier',
+	long_description=_read('README.md'),
+	long_description_content_type='text/markdown',
+	url='https://github.com/cfv-project/cfv',
+	author='David Gnedt (Current Maintainer)',
+	author_email='cfv-project AT davizone DOT at',
+	license='GPL',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Console',
+		'Topic :: System :: Archiving',
+		'Topic :: Utilities',
+		'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+	],
+	keywords='cfv checksum verify sfv csv crc bsdmd5 md5sum sha1sum sha224sum sha256sum sha384sum sha512sum torrent par par2',
+	project_urls={
+		'Bug Tracker': 'https://github.com/cfv-project/cfv/issues',
+		'Source Code': 'https://github.com/cfv-project/cfv',
+		'Original Project': 'http://cfv.sourceforge.net/',
+	},
+	python_requires='>=2.7, <3',
+	packages=find_packages('lib'),
+	package_dir={'': 'lib'},
+	data_files=[('man/man1', ['cfv.1'])],
+	entry_points={
+		'console_scripts': [
+			'cfv=cfv.common:main',
+		],
+	},
 )
