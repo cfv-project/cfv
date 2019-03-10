9819cd79caae398f373ac4c7dee9a2292225b022
David Gnedt <david@gnedt.at>
2019-03-11 00:52:58 +0100

    Remove old entry point scripts as they are now created by setuptools console_scripts

--- bin/cfv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem use this line for old windows (95,98,ME)
-rem c:\python24\python -c "import cfv; cfv.main()" %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-rem use this line for new windows (NT, 2000, XP, etc)
-c:\python24\python -c "import cfv; cfv.main()" %*
