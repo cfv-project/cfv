b08b0313dd596598921ae9eae0948117aa26aad3
David Gnedt <david@gnedt.at>
2019-03-11 04:26:19 +0100

    Bump version

--- lib/cfv/common.py
+++ lib/cfv/common.py
@@ -397,7 +397,7 @@ def make_rename_formatmap(l_filename):
 	sp=os.path.splitext(l_filename)
 	return {'name':sp[0], 'ext':sp[1], 'fullname':l_filename}
 
-__version__='2.0.0.dev1'
+__version__='2.0.0.dev2'
 __homepage__='https://github.com/cfv-project/cfv'
 
 _hassymlinks=hasattr(os,'symlink')
