5673ef15bc15a1c87de57bb088d0128e53952a7a
Steven Xu <stevenxxiu@gmail.com>
2020-01-05 00:21:32 +1100

    Unused imports

--- lib/cfv/strutil.py
+++ lib/cfv/strutil.py
@@ -1,10 +1,7 @@
 from future import standard_library
 standard_library.install_aliases()
 from builtins import str
-from builtins import object
-import codecs
 import unicodedata
-from io import StringIO
 
 
 def safesort(l):
