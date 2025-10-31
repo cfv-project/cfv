9c8ac2706856cd9e3ac0dcae62a4463424449479
Lisa Gnedt <lisa@gnedt.at>
2025-10-31 01:35:02 +0100

    Add deprecated license field for backward compatibility with older setuptools versions
    The license field was deprecated and replaced by the license-expression
    field. However, older setuptools versions still require the license
    field:
    
    distutils.errors.DistutilsOptionError: No configuration found for dynamic 'license'.
    Some dynamic fields need to be specified via `tool.setuptools.dynamic`
    others must be specified via the equivalent attribute in `setup.py`.

--- setup.py
+++ setup.py
@@ -27,9 +27,10 @@ setup(
     version=version,
     author='Lisa Gnedt (Current Maintainer)',
     author_email='%s@%s' % ('cfv-project', 'davizone.at'),
-    # license_expression is defined here and not in pyproject.toml due to
-    # backward compatibility issues with older setuptools versions.
+    # license and license_expression is defined here and not in pyproject.toml
+    # due to backward compatibility issues with older setuptools versions.
     # See: https://github.com/pypa/setuptools/issues/4903
+    license='GPL-2.0-or-later AND MIT',
     license_expression='GPL-2.0-or-later AND MIT',
     license_files=('COPYING', 'lib/cfv/BitTorrent/LICENSE.txt'),
 )
